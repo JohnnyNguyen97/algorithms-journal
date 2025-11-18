c9f2768e641dd7fe0e19acde0ba7d30bcfe342a9
Johnny Hoang Dinh Huy Nguyen <jhdn48823@edu.ucl.dk>
2025-11-18 21:18:37 +0100

    Function for fibonacci from geeksforgeeks

--- notes/Recursion.py
+++ notes/Recursion.py
@@ -22,3 +22,29 @@ def look_for_key(box):
 # especially when dealing with nested structures like boxes within boxes.
 
         
+
+# Function for fibonacci
+def fib(n):
+
+    # Stop condition
+    if (n == 0):
+        return 0
+
+    # Stop condition
+    if (n == 1 or n == 2):
+        return 1
+
+    # Recursion function
+    else:
+        return (fib(n - 1) + fib(n - 2))
+
+
+
+# Initialize variable n.
+n = 5;
+print("Fibonacci series of 5 numbers is :",end=" ")
+
+# for loop to print the fibonacci series.
+for i in range(0, n): 
+
+    print(fib(i),end=" ")
